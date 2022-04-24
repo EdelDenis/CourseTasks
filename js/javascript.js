d98012fe87390918f5db0c00d7b5e65a9e116140
EdelDenis <edeldenis12@gmail.com>
2022-04-24 15:23:25 +0000

    h

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,9 @@
 "use strict"
 
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+while(numberOfFilms == null || numberOfFilms == 0){
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+};
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -14,12 +17,23 @@ let personalMovieDB = {
 for(let i=0;i<2;i++){
 let questionA = prompt("Один из последних просмореных фильмов?", "");
 let questionB = prompt("На сколько оцените его?", "");
-if (questionA != null && questionB != null && questionA != "" && questionB != "" && questionA < 50){
+if (questionA != null && questionB != null && questionA != "" && questionB != "" && questionA.length < 50){
     personalMovieDB.movies[questionA] = questionB;
     console.log("done");
+} else {
+    console.log("Error")
+    i--;
 }
-
-
 };
+
+if(personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов")
+} else if(personalMovieDB.count >=10 && personalMovieDB.count <=30){
+    alert("Вы классчический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else{
+    alert("Произошла ошибка")
+}
 
 console.log(personalMovieDB);
