ca3470a51f66bce53da358067d36e68c6f0a4c50
EdelDenis <edeldenis12@gmail.com>
2022-04-24 15:00:35 +0000

    h

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,25 @@
 "use strict"
 
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+for(let i=0;i<2;i++){
+let questionA = prompt("Один из последних просмореных фильмов?", "");
+let questionB = prompt("На сколько оцените его?", "");
+if (questionA != null && questionB != null && questionA != "" && questionB != "" && questionA < 50){
+    personalMovieDB.movies[questionA] = questionB;
+    console.log("done");
+}
+
+
+};
+
+console.log(personalMovieDB);
